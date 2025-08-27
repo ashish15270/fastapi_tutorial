2e4af7e8ee8c53f5ed6e10d541f3daf427747c2c
ashish kumar <a.ashish01kumar@gmail.com>
2025-08-27 18:28:34 +0530

    full stack

--- todo_app/main.py
+++ todo_app/main.py
@@ -1,12 +1,19 @@
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import models
 from database import engine
 from routers import auth, todos, admin, users
+from fastapi import templating
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+templates=templating.Jinja2Templates(directory='todo_app/templates')
+
+app.get('/')
+def test(request: Request):
+    return templates.TemplateResponse('home.html', {'request': request})
 
 app.include_router(auth.router)
 app.include_router(todos.router)
